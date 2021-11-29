6f18d83842490f5f6a79d2b8a7d687968dfbc60e
Millena Gomes <mrb@lionx.com.br>
2021-11-29 17:23:40 -0300

    Learning More about Javascript advanced

---
+++ IncludesJavascript/script.js
@@ -0,0 +1,17 @@
+//includes
+let names = ["Matheus", "Lucas", "Joao"];
+console.log(names.includes("Luca"));
+
+if (nomes.includes("Matheus")) {
+  console.log("MATHEUS ESTA NA LISTA");
+} else {
+  console.log("MATHEUS NÃO ESTA NA LISTA");
+}
+
+//endsWith
+let names = "Matheus";
+console.log(names.endsWith("s"));
+
+//startsWith
+let names = "Matheus";
+console.log(names.startsWith("MAT"));
