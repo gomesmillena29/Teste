4f9b2ba1253b731962feb39aab3f2b463eca51c5
Millena Gomes <mrb@lionx.com.br>
2021-11-29 16:46:47 -0300

    class concept

--- FunctionAnonymous/script.js
+++ FunctionAnonymous/script.js
@@ -1,4 +1,4 @@
-function adicionar(...numbers) {
+/*function adicionar(...numbers) {
   let total = numbers.reduce((total, proximo) => {
     let soma = total + proximo;
     return soma;
@@ -6,3 +6,38 @@ function adicionar(...numbers) {
   console.log(total);
 }
 adicionar(1, 2, 3, 4, 5, 8, 9);
+*/
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+
+class ListTarefas extends List {
+  constructor() {
+    super();
+    this.nome = "Matheus;";
+  }
+
+  mostrarNome() {
+    console.log(this.nome);
+  }
+
+  addTarefa() {
+    this.tarefas.push("Nova Tarefa");
+    console.log(this.tarefas);
+  }
+}
+
+const minhasTarefas = new ListTarefas();
+
+document.getElementById("novo").onclick = function () {
+  minhasTarefas.add("Minha Tarefa");
+};
+
+minhasTarefas.mostrarNome();
