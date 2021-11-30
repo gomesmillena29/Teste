17cdbde6183f81c4c2c0eecb98d8e07d3b98d036
Millena Gomes <mrb@lionx.com.br>
2021-11-30 14:15:30 -0300

    fix: codigo review

--- Repository/script.js
+++ Repository/script.js
@@ -10,29 +10,28 @@ buscaRepo = async () => {
     return;
   }
 
-  const hasRepo = repositorios.find(repo => repo.full_name === input.value);
+  const hasRepo = repositorios.find((repo) => repo.full_name === input.value);
 
-  if(hasRepo){
+  if (hasRepo) {
     alert("Este repositorio ja esta na lista!");
     return;
   }
 
-  const response = await fetch(`https://api.github.com/repos/${input.value}`);
-                   .then(res => res.json()
-                   .then(json => {
-                     console.log(json);
-                     repositorios.push(json);
-                     input.value = "";
-                     renderRepos();
-                   })
-}
+  const response = await fetch(`https://api.github.com/repos/${input.value}`)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      repositorios.push(json);
+      input.value = "";
+      renderRepos();
+    });
+};
 
-function renderRepos(){
-
+function renderRepos() {
   listElement.innerHTML = "";
-  for(repo of repositorios){
+  for (repo of repositorios) {
     let repoElement = document.createElement("li");
-    repoElement.setAttribute("style", "text-decoration: nome; list-style:none;")
+    repoElement.setAttribute("style", "text-decoration: nome; list-style:none;");
 
     let avatarElement = document.createElement("img");
     avatarElement.src = respo.owner.avatar_url;
@@ -43,19 +42,17 @@ function renderRepos(){
 
     let posicao = repositorios.indexOf(repo);
 
-    linkElement.setAttribute("onclick", "deletarRepo()")
-
+    linkElement.setAttribute("onclick", "deletarRepo()");
 
     let name = document.createTextNode(repo.full_name);
     repoElement.appendChild(name);
     repoElement.appendChild(avatarElement);
     repoElement.appendChild(linkElement);
     listElement.appendChild(repoElement);
-
   }
 }
 
-function deletarRepo(posicao){
+function deletarRepo(posicao) {
   repositorios.splice(posicao, 1);
   renderRepos();
 }
